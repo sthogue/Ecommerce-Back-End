8fb87ae983b45a0e936b8dbd99a0799dcde5fbeb
sthogue <sthogue16@gmail.com>
2023-08-31 16:05:05 -0500

    linted & fixed files

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -37,8 +37,8 @@ Product.init(
     category_id:{
       type: DataTypes.INTEGER,
       references:{
-        model: 'category',
-        key: 'id'
+        model: "category",
+        key: "id"
       }
     }
   },
@@ -47,7 +47,7 @@ Product.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
@@ -16,15 +16,15 @@ ProductTag.init(
     product_id:{
       type:DataTypes.INTEGER,
       references:{
-        model:'Product',
-        key:'id'
+        model:"Product",
+        key:"id"
       }
     },
     tag_id:{
       type:DataTypes.INTEGER,
       references:{
-        model:'Tag',
-        key:'id'
+        model:"Tag",
+        key:"id"
       }
     }
   },
@@ -33,7 +33,7 @@ ProductTag.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
