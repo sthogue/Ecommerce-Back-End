3133651a58705b0b6efb20d719852ca48411f1bf
sthogue <sthogue16@gmail.com>
2023-08-31 15:17:56 -0500

    added routers for category, product, and tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,67 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try{
+    const tagData = await Tag.findAll({
+      //includes its associated Product data
+      include:[{model:Product, as: 'products'}]
+    });
+    res.status(200).json(tagData);
+  } catch(err){
+    res.status(500).json(err);
+  } 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try{
+    const tagData = await Tag.findByPk(req.params.id, {
+      //includes its associated Product data
+      include:[{model:Product, as: 'products'}]
+    });
+    res.status(200).json(tagData);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      tag_name:req.tag_name});
+    res.status(200).json(tagData);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try{
   // update a tag's name by its `id` value
+  const tagData = await Tag.update(
+    {tag_name:req.body.tag_name},
+    {where:{id:req.params.id}}
+  );
+  res.status(200).json(tagData);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
+  
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const tagData = await Tag.destroy({
+      where:{id:req.params.id}
+    });
+    res.status(200).json(tagData);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
