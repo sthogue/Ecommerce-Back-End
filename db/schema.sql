1889e9e286e2ebd54ad4edf21220cd6997d0e364
sthogue <sthogue16@gmail.com>
2023-08-31 12:45:28 -0500

    updated schema.sql to specifications

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,32 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- Create Categories
+CREATE TABLE Category (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL 
+);
+
+-- Create Products
+CREATE TABLE Product (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT DEFAULT "10",
+    category_id INT,
+    FOREIGN KEY (category_id)
+    REFERENCES categories(id)
+);
+
+CREATE TABLE Tag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(50)
+);
+
+CREATE TABLE ProductTag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES Product(id),
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)
+);
