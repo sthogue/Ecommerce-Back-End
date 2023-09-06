be3287475f0a0a3d6965575ffd02f1eb773ffa96
sthogue <sthogue16@gmail.com>
2023-09-05 22:45:59 -0500

    added body to req.body.tag_name

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,7 +33,7 @@ router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tagData = await Tag.create({
-      tag_name:req.tag_name});
+      tag_name: req.body.tag_name});
     res.status(200).json(tagData);
   } catch(err){
     res.status(500).json(err);
